32c1970860861616f90ad1c256540cefdf781bf3
Zac Butko <zac.butko@gmail.com>
2022-06-02 22:44:31 -0700

    Jest passing

--- packages/esm-patient-registration-app/src/root.test.tsx
+++ packages/esm-patient-registration-app/src/root.test.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root.component';
-import { match } from 'react-router-dom';
 
 window['getOpenmrsSpaBase'] = jest.fn().mockImplementation(() => '/');
-
-const path = `/patient/:patientUuid/edit`;
-const sampleMatchProp: match<{ patientUuid: string }> = {
-  isExact: false,
-  path,
-  url: path.replace(':patientUuid', '1'),
-  params: { patientUuid: '1' },
-};
 
 describe('root component', () => {
   it('renders without crashing', () => {
@@ -19,11 +10,11 @@ describe('root component', () => {
     ReactDOM.render(
       <Root
         savePatientForm={jest.fn()}
-        match={sampleMatchProp}
         addressTemplate={{ results: [] }}
         currentSession={{} as any}
-        patientIdentifiers={[]}
         relationshipTypes={{ results: [] }}
+        identifierTypes={[]}
+        isOffline={false}
       />,
       div,
     );
